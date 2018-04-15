a80ea7e327f13d6f534d38c55c436fbee07021e2
joyce <joyce09954@gmail.com>
2018-04-15 20:15:25 +0800

    lyric color change with video

--- Video.js
+++ Video.js
@@ -37,7 +37,7 @@ var count = Object.keys(obj.lyrics).length;
 for(var i = 0; i < count; i++){
   var small_sub =  obj.lyrics[i].l;
   //console.log(small_sub);
-  var subtitle = $("#subtitle").append('<li class = "arc'+ i +'" ><i class="far fa-play-circle" onclick="playAt('+obj.lyrics[i].t+','+obj.lyrics[i].d+')"></i> '+small_sub+'</li>'); 
+  var subtitle = $("#subtitle").append('<li class = "arc'+ i +'" ><i class="far fa-play-circle" id = "icon'+i+'"  onclick="playAt('+obj.lyrics[i].t+','+obj.lyrics[i].d+')"></i> <span id = "small_sub'+i+'">'+small_sub+'</span></li>'); 
 }
 
 // var ssss = '';
@@ -137,9 +137,65 @@ function playAt(st, dr) {
 
 
 
+var delta;//evt.deltaY || evt.wheelDelta || evt.detail;
+
+function MouseWheel (e) {
+  e = e || window.event;
+  // alert(['scrolled ', (( e.wheelDelta <= 0 || e.detail > 0) ? 'down' : 'up')].join(''));
+  delta = e.deltaY;
+  console.log("in func" + delta);
+  return e;
+}
+
+// hook event listener on window object
+if ('onmousewheel' in window) {
+  window.onmousewheel = MouseWheel;
+} else if ('onmousewheel' in document) {
+  document.onmousewheel = MouseWheel;
+} else if ('addEventListener' in window) {
+  window.addEventListener("mousewheel", MouseWheel, false);
+  window.addEventListener("DOMMouseScroll", MouseWheel, false);
+}
+
+
+
+
+// detect event model
+// if (window.addEventListener) { // for modern browser
+//   _addEventListener = "addEventListener";
+// } else { // for IE 8 or older
+//   _addEventListener = "attachEvent";
+// }
+
+// // detect available wheel event
+// if (document.onmousewheel === null
+//     && window.addEventListener === undefined) {
+//   // IE 8 or older
+//   wheel_event_name = 'onmousewheel';
+// } else if (Modernizr.hasEvent('wheel') || (!!window.WheelEvent && !!window.MouseWheelEvent)) {
+//   // detect if browser has DOM L3 wheel event: Firefox 17 and IE 9 or later version
+//   wheel_event_name = 'wheel';
+//   inverse_wheel_direction = true;
+// } else if (!!window.WheelEvent) {
+//   // Safari, Chrome
+//   wheel_event_name = 'mousewheel';
+// } else {
+//   // Firefox 16 and earlier version
+//   wheel_event_name = 'DOMMouseScroll';
+//   inverse_wheel_direction = true;
+// }
+// document[_addEventListener](wheel_event_name, wheelHandler);
+
+
+
+
+
+console.log(delta);
+
+
 
 var idx = 0;
-var now_index = -1;
+var now_index = 0;
 var s_tmp = 0;
 var r = $('#subtitle').position();
 function put_titie(t){
@@ -156,16 +212,29 @@ function put_titie(t){
          }
        }
        if(obj.lyrics[idx].t <= t*1000){
+         document.getElementById("icon"+idx).style.color="green";
+        //  console.log("idx out loop : "+idx);
+
+         for(var a = 0;a<count;a++){
+          //  console.log("idx in loop : "+idx);
+          //  console.log("a : "+a);
+           
+           if(a === idx)
+            document.getElementById("small_sub"+a).style.backgroundColor="rgba(121, 120, 120, 0.3)";            
+           else
+            document.getElementById("small_sub"+a).style.backgroundColor="#e6e6e6";
+         }
+         
          var small_sub =  obj.lyrics[idx].l + '</br>';
          document.getElementById("under_subtitle").innerHTML=small_sub;
-          //console.log(idx)
-          if(now_index!=idx){
+          //console.log(idx)  //now_index!=idx
+          if(now_index < idx-2 || now_index > idx){
             console.log(now_index + ' '+ idx);
-            s = $('ul.arcchang li.arc'+idx).position();
+            s = $('ul.arcchang li.arc'+(idx-2)).position();
             console.log(s);
             s_tmp+=s['top']-r['top'];
             console.log(s_tmp);
-            now_index = idx;
+            now_index++;
             $('#subtitle').animate({scrollTop: s_tmp},0);
           }
           //console.log(s)
